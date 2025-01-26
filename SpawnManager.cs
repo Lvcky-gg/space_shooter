ec8c303ebcc66d2dfdeb3f4e187d8cfad5af08ab
Your Name <matadorcrab@gmail.com>
2025-01-26 16:16:14 -0500

    chore: dmg

--- SpawnManager.cs
+++ SpawnManager.cs
@@ -7,6 +7,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
+    private GameObject _triple_shot;
+    [SerializeField]
     private GameObject _enemyContainer;
 
     private bool _stopSpawning = false;
@@ -14,6 +16,7 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnTriple());
     }
 
 
@@ -30,7 +33,20 @@ public class SpawnManager : MonoBehaviour
             Vector3 posToSpawn = new Vector3(Random.Range(-8f,8f),7,0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(5.0f);
+            yield return new WaitForSeconds(Random.Range(1f,5f));
+        }
+    }
+
+    IEnumerator SpawnTriple()
+    {
+
+        while (_stopSpawning == false)
+        {
+
+            yield return new WaitForSeconds(Random.Range(10f, 40f));
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newTriple = Instantiate(_triple_shot, posToSpawn, Quaternion.identity);
+            newTriple.transform.parent = _enemyContainer.transform;
         }
     }
     public void OnPlayerDeath()
